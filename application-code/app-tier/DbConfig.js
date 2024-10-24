ad91622df848901200ae757205e5db1286cf9038
joyboy <akshaysamminga@gmail.com>
2024-10-24 16:41:57 +0530

    third

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'database-1-3tier.clogouqck9vn.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'YourPassword',
+    DB_PWD : 'akshay123',
     DB_DATABASE : 'webappdb'
 });
